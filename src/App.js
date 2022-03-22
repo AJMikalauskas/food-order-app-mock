ff800b0a0fb92a9e530672e9882566a293f74eb5
AJMikCode <ajmikalauskas@hotmail.com>
2022-03-22 11:07:19 -0500

    Changed Meal Data to be called from Firebase DB, added via POST button, GET call in AvailableMeals.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,109 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header/Header';
-import { Fragment } from 'react';
+import { Fragment, useState, useEffect, useCallback, useMemo } from 'react';
 import MealItemForm from './Meal-Items/AvailableMeals';
 import MealsSummary from './Meals-Summary/MealsSummary';
 import AvailableMeals from './Meal-Items/AvailableMeals';
 
 function App() {
-  const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+  // const DUMMY_MEALS = [
+  //   {
+  //     id: 'm1',
+  //     name: 'Sushi',
+  //     description: 'Finest fish and veggies',
+  //     price: 22.99,
+  //   },
+  //   {
+  //     id: 'm2',
+  //     name: 'Schnitzel',
+  //     description: 'A german specialty!',
+  //     price: 16.5,
+  //   },
+  //   {
+  //     id: 'm3',
+  //     name: 'Barbecue Burger',
+  //     description: 'American, raw, meaty',
+  //     price: 12.99,
+  //   },
+  //   {
+  //     id: 'm4',
+  //     name: 'Green Bowl',
+  //     description: 'Healthy...and green...',
+  //     price: 18.99,
+  //   },
+  // ];
+
+  //Error and isLoading States
+ // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  // const [apiData, setApiData] = useState([]);
+
+  //   // This is the POST request to just get the data to the google firebase DB
+  // //const postMealsData = async() => {
+  // // const backendStoredDummyMeals = await fetch("https://food-ordering-app-db-b3d2e-default-rtdb.firebaseio.com/meals.json",
+  // // {
+  // //   method:'POST',
+  // //   headers: {
+  // //     'Content-Type':
+  // //       'application/json;charset=utf-8'
+  // //   },
+  // //   body: JSON.stringify({
+  // //     //id is the randomized cryptic key 
+  // //     "name": "Green Bowl",
+  // //     "description": "Healthy...and green...",
+  // //     "price":  18.99
+  // //   })
+  // // });
+  // // const data = await backendStoredDummyMeals.json();
+  // // console.log(data);
+  // // }
+
+
+  // // GET Request
+  // const getMealsData = useCallback(async() => {
+  //   setLoading(true);
+  //   setError(null);
+  //   try 
+  //   {
+  // const response = await fetch("https://food-ordering-app-db-b3d2e-default-rtdb.firebaseio.com/meals.json");
+  // const data = await response.json(); 
+  // console.log(data);
+
+  // const mealsFromFirebaseDB = [];
+  // for(const key in data)
+  // {
+  //   mealsFromFirebaseDB.push({
+  //     id: key,
+  //     name: data[key].name,
+  //     description: data[key].description,
+  //     price: data[key].price
+  //   })
+  // }
+  // setApiData(mealsFromFirebaseDB);
+  //   }
+  //   catch(error)
+  //   {
+  //     setError(error.message);
+  //   }
+  //   // Stop Loading state once data is shown via this end statement
+  //   setLoading(false);
+  // }, []);
+  // // Need to use useCallback so that the function isn't being referenced as new function everytime it's called in the useEffect()
+
+  // useEffect(() => {
+  //   getMealsData();
+  // }, [getMealsData]);
 
   return (
     <Fragment>
       <Header />
       <MealsSummary />
-      <AvailableMeals meals={DUMMY_MEALS}/>
+      {/* meals={DUMMY_MEALS}  -  No more need for this since HTTP calls are made in Available Meals.js itself*/}
+      <AvailableMeals />
+      {/* <button onClick={postMealsData}>POST Meals Data</button> */}
+      {/* This  */}
+      {/* <button onClick={getMealsData}>GET Meals Data</button> */}
     </Fragment>
     // <div className="App">
     //   <header className="App-header">
--- src/Meal-Items/AvailableMeals.js
+++ src/Meal-Items/AvailableMeals.js
@@ -1,16 +1,81 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useCallback, useEffect } from "react";
 import styles from './AvailableMeals.module.css';
 import MealItem from "./MealItem";
 import Card from "../UI/Card";
 import MealItemForm from "./MealItemForm";
 
 const AvailableMeals = (props) => {
-  
+  // HTTP get call and store data in a useState
+  const [error,setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [apiData, setApiData] = useState([]);
+
+    // This is the POST request to just get the data to the google firebase DB
+  //const postMealsData = async() => {
+  // const backendStoredDummyMeals = await fetch("https://food-ordering-app-db-b3d2e-default-rtdb.firebaseio.com/meals.json",
+  // {
+  //   method:'POST',
+  //   headers: {
+  //     'Content-Type':
+  //       'application/json;charset=utf-8'
+  //   },
+  //   body: JSON.stringify({
+  //     //id is the randomized cryptic key 
+  //     "name": "Green Bowl",
+  //     "description": "Healthy...and green...",
+  //     "price":  18.99
+  //   })
+  // });
+  // const data = await backendStoredDummyMeals.json();
+  // console.log(data);
+  // }
+
+
+  // GET Request
+  const getMealsData = useCallback(async() => {
+    setLoading(true);
+    setError(null);
+    try 
+    {
+  const response = await fetch("https://food-ordering-app-db-b3d2e-default-rtdb.firebaseio.com/meals.json");
+  const data = await response.json(); 
+  console.log(data);
+
+  const mealsFromFirebaseDB = [];
+  for(const key in data)
+  {
+    mealsFromFirebaseDB.push({
+      id: key,
+      name: data[key].name,
+      description: data[key].description,
+      price: data[key].price
+    })
+  }
+  console.log(mealsFromFirebaseDB);
+  setApiData(mealsFromFirebaseDB);
+    }
+    catch(error)
+    {
+      setError(error.message);
+    }
+    // Stop Loading state once data is shown via this end statement
+    setLoading(false);
+  }, []);
+  // Need to use useCallback so that the function isn't being referenced as new function everytime it's called in the useEffect()
+
+  useEffect(() => {
+    getMealsData();
+  }, [getMealsData]);
+
+
+
   // This calls MealItem.js 4 times
     // This is useful beacuse it is a way that the MealItem.js props info can be called specifically through the context.
       // Each List Item has props specifically from the MealItem.js file 
+        // since mealsFromFirebaseDB is set in the useCallback, not accesible in function, useState outside of it and set to the array inside
+          // use the apiData.map to show on the screen all the information and send to MealItem.js
   const dummyMealsList = 
-  props.meals.map((meal) => (
+  apiData.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
@@ -19,12 +84,13 @@ const AvailableMeals = (props) => {
       mealPrice={meal.price}
     />
   ));
-  console.log(props.meals);
-  console.log(dummyMealsList[0].props);
-  if(dummyMealsList[0].props.mealId === 'm1')
-  {
-    console.log("THis contains sushi and y...")
-  }
+    // Error if these are in, all we need now is http calls not props anymore 
+  // console.log(props.meals);
+  // console.log(dummyMealsList[0].props);
+  // if(dummyMealsList[0].props.mealId === 'm1')
+  // {
+  //   console.log("THis contains sushi and y...")
+  // }
 
   return (
       <Card className={styles.meals}>
